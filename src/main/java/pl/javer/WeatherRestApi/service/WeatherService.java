052ebcc69402560ae2b1b6dae5a1aa414cafcf3f
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-28 14:10:10 +0100

    check weatherService with APIKEY

--- src/main/java/pl/javer/WeatherRestApi/service/WeatherService.java
+++ src/main/java/pl/javer/WeatherRestApi/service/WeatherService.java
@@ -1,12 +1,22 @@
 package pl.javer.WeatherRestApi.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import pl.javer.WeatherRestApi.model.WeatherDto;
 
 @Service
+@Slf4j
 public class WeatherService {
 
+    private static final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/";
+    private static final String API_KEY = "9475ef3eddb4ee346fa3072af4de6e99";
+    private RestTemplate restTemplate = new RestTemplate();
+
     public WeatherDto getWeather() {
+        String response = restTemplate.getForObject(WEATHER_URL + "weather?q={city}&appid={apiKey}&units=metric&lang=pl",
+                String.class, "lublin", API_KEY);
+        log.info(response);
         return null;
     }
 }
